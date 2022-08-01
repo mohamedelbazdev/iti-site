1160c9908b49484946aec4b542325e43625d043c
Mohamed Elbaz <mohamedelbazdev@gmail.com>
2022-08-01 19:06:57 +0200

    layout & create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ProvidersComponent } from './pages/providers/providers.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
+import { SliderComponent } from './pages/home/slider/slider.component';
+import { CategoryComponent } from './pages/home/category/category.component';
+import { ProviderComponent } from './pages/home/provider/provider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CheckoutComponent,
+    ProvidersComponent,
+    CategoriesComponent,
+    FavoriteComponent,
+    SliderComponent,
+    CategoryComponent,
+    ProviderComponent,
   ],
   imports: [
     BrowserModule,

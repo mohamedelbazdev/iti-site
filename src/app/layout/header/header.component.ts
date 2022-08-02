811582565fd953a3d551713f4b536ac156ec326b
aya3omira <104975497+aya3omira@users.noreply.github.com>
2022-08-03 01:05:54 +0200

    Add log out

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() closeMenu: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private auth: AuthService,private router: Router) {
+      this.loginForm = new FormGroup({
+          email: new FormControl('', [Validators.required, Validators.email]),
+          password: new FormControl('', [Validators.required])
+      });
   }
+
+
+
+
+  logout(){
+    this.auth.logoutUser()
+}
+
+isLogin():boolean{
+   return this.auth.loggedIn()
+}
+
+onSubmit() {
+    if(this.loginForm.valid) {
+        this.auth.loginUser(this.loginForm.value).subscribe((res:any) => {
+            this.auth.setToken(res.token)
+            this.router.navigateByUrl('/')
+        })
+    }
+}
 
 }

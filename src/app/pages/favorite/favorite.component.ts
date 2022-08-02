7b59575ad7ee0db0ce22a43ec10cdbb71453ccd2
Ismail <ismailmagdy8@gmail.com>
2022-08-02 03:47:51 +0200

    order
    order

--- src/app/pages/favorite/favorite.component.ts
+++ src/app/pages/favorite/favorite.component.ts
@@ -10,6 +10,28 @@ export class FavoriteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.featuredfavorites = [{
+      "id": 1,
+      "provider_id": 1,
+      "user_id": 2,
+      "created_at": null,
+      "updated_at": null,
+      "providers": {
+          "id": 2,
+          "name": "provider",
+          "image": "assets/img/cat-1.jpg",
+          "email": "provider@provider.com",
+          "mobile": "01553524888",
+          "email_verified_at": null,
+          "lat": null,
+          "lng": null,
+          "status": 1,
+          "role_id": 2,
+          "created_at": null,
+          "updated_at": null
+      }
+  }]
 
+
+}
 }

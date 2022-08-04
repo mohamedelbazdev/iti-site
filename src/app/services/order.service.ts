0600eaeca451554b3a0fbaa4a6c205cdc224da3a
Ismail <ismailmagdy8@gmail.com>
2022-08-04 02:38:27 +0200

    add
    add

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  public orderUrl = "http://127.0.0.1:8000/api/users/orders";
+
+  constructor(private http:HttpClient) { }
+
+  geTorders() {
+    return this.http.get<any>(this.orderUrl)
+  }
+
 }
+

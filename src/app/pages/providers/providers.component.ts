03dff650c7f8f7045b8b8751ca206d64ee90d536
Ismail <ismailmagdy8@gmail.com>
2022-08-06 20:23:57 +0200

    update
    update

--- src/app/pages/providers/providers.component.ts
+++ src/app/pages/providers/providers.component.ts
@@ -2,33 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { ProviderService } from "../../services/provider.service";
 import { FavoriteService } from "../../services/favorite.service";
 import {Router} from "@angular/router";
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-providers',
   templateUrl: './providers.component.html',
   styleUrls: ['./providers.component.css']
 })
 export class ProvidersComponent implements OnInit {
-  featuredproviders!: any[];
+  providers!: any[];
 
 
 
   constructor(
               private provider: ProviderService,
               private favorite: FavoriteService,
-              private router: Router
-              ) { }
+              private router: Router,
+              private toastr: ToastrService
+              ){ }
 
   ngOnInit(): void {
 
     this.provider.getProviders().subscribe(res => {
-      this.featuredproviders = res.data
+      this.providers = res.data
     })
   }
 
 
   createFav(providerId:number){
     this.favorite.create(providerId).subscribe(() => {
-      this.router.navigateByUrl('/Favorites')
+      // this.router.navigateByUrl('/Favorites')
+      this.toastr.success('Add to favorite sucessed', ':)');
     })
   }
 

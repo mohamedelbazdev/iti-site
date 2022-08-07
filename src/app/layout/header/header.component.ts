9d240a34a838920a42ad12140b3454e7d104495a
Mohamed Elbaz <mohamedelbazdev@gmail.com>
2022-08-07 17:43:55 +0200

    edit

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 
@@ -9,20 +9,23 @@ import { AuthService } from 'src/app/services/auth.service';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     @Output() closeMenu: EventEmitter<void> = new EventEmitter<void>();
-
+    username: string = 'USer';
     loginForm: FormGroup;
 
-    constructor(private auth: AuthService, private router: Router) {
+    constructor(private auth: AuthService, private router: Router, private route: ActivatedRoute,) {
         this.loginForm = new FormGroup({
             email: new FormControl('', [Validators.required, Validators.email]),
             password: new FormControl('', [Validators.required])
         });
     }
 
-
-
+    ngOnInit(): void {
+        this.auth.setUser(this.route.snapshot.params['id']).subscribe(res => {
+            this.username = res.data
+        })
+    }
 
     logout() {
         this.auth.logoutUser()
@@ -42,3 +45,7 @@ export class HeaderComponent {
     }
 
 }
+function user(user: any, Object: ObjectConstructor) {
+    throw new Error('Function not implemented.');
+}
+

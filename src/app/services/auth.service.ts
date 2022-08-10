a8aa297ae32ac36dd1c9885e0bbd0d513e1a8d0a
aya3omira <104975497+aya3omira@users.noreply.github.com>
2022-08-10 18:04:34 +0200

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -58,4 +58,14 @@ export class AuthService {
     return !!localStorage.getItem('token')
   }
 
-}
+  // isUser() {
+  //   return JSON.parse(localStorage.getItem('user'))?.role_id == 3
+
+  // }
+  // isAuthenticated(): boolean {
+  //   let data = localStorage.getItem('currentUser');
+  //   this.token = JSON.parse(data) && JSON.parse(data).token;
+  //   this.userRole = JSON.parse(data) && JSON.parse(data).role;
+  //   return !!this.token;
+  }
+

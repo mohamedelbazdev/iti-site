779b836bce8d49b262857d21ac78af24040052da
Ismail <ismailmagdy8@gmail.com>
2022-08-12 02:47:06 +0200

    add
    add

--- src/app/pages/prov-order-details/prov-order-details.component.ts
+++ src/app/pages/prov-order-details/prov-order-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../../services/order.service'
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-prov-order-details',
   templateUrl: './prov-order-details.component.html',
@@ -8,6 +10,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class ProvOrderDetailsComponent implements OnInit {
 
+  id: any;
   showOrder: any = {}
 
   lat: any = 0.00
@@ -28,13 +31,22 @@ export class ProvOrderDetailsComponent implements OnInit {
     lat: this.lat,
     lng: this.lng
   };
-  constructor(private order: OrderService) { }
+  constructor(
+              private order: OrderService,
+              private router: Router,
+              private route: ActivatedRoute,
+              ) { }
 
   ngOnInit(): void {
 
   this.order.getOrders().subscribe(res => {
     this.showOrder = res.data
   })
+
+  this.order.showOrder(this.route.snapshot.params['id']).subscribe(res => {
+    this.showOrder = res.data
+  })
 }
+
 
 }
